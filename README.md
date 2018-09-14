7e0382b550e6347a49797b9b656375cce4a908f2
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-14 10:49:19 -0500

    add conversion to lower case

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Python-OOP-Word_Guessing
+
+# Word Guessing Game
+I decided to create my JavaScript class word guessing game using python.
+
+![Demo](/word_guessing.gif)
--- game.py
+++ game.py
@@ -11,7 +11,7 @@ class Game_controller:
         import random
         random_word = random.choice(words_bank.words_list)
         cls.new_word = word.Word(random_word)
-        # print(cls.new_word.word) # for testing only
+        print(cls.new_word.word) # for testing only
         cls.new_word.split_letters()
         cls.new_word.display_word()
         cls.take_user_guess()
@@ -21,7 +21,7 @@ class Game_controller:
     def take_user_guess(cls):
         while not cls.new_word.status:
             guess = input('Guess a letter ')
-            cls.new_word.take_char(guess)
+            cls.new_word.take_char(guess.lower())
             cls.new_word.track_status()
             cls.new_word.display_word()
         cls.score += 1
@@ -32,7 +32,7 @@ class Game_controller:
     @classmethod
     def reset_game(cls):
         confirm = input('WOULD YOU LIKE TO PLAY AGAIN? (y/n) ')
-        if confirm == 'y':
+        if confirm == 'y' or 'Y':
             cls.generate_word()
         return print('THANKS FOR PLAYING!')
 
